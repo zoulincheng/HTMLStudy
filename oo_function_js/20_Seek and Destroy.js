5ad4c57af9dbb9fb16420ca1cd3b1d09813767ca
zoulincheng <99linzi@163.com>
2016-05-16 21:59:47 +0800

    add new js code

--- oo_function_js/20_Seek and Destroy.js
+++ oo_function_js/20_Seek and Destroy.js
@@ -23,22 +23,65 @@ destroyer([1, 2, 3, 1, 2, 3], 2, 3);
 
 
 
+// Define a callback function.
+function CheckIfPrime(value, index, ar) {
+  high = Math.floor(Math.sqrt(value)) + 1;
+  console.log("high="+high);
 
-function destroyer(arr) {
-	var args = Array.from(arguments);
-	var arrA = [];
-	console.log(args);
-	console.log(args.length);
-  // Remove all the values
-  	for (var i = 1; i < args.length; i++){
-  		for (var j = 0; j < arr.length; j++){
-  			if (arr[j] != args[i]){
-  				arrA.push(arr[j]);
-  			}
-  		}
-  	}
-  	console.log(arrA);
-    return arrA;
+    for (var div = 2; div <= high; div++) {
+        if (value % div == 0) {
+            return false;
+        }
+    } 
+    return true;
 }
 
+
+
+function filterNum(value, index, arr){
+    var args = (arguments.length === 1 ? [arguments[0]] : Array.apply(null, arguments));
+    console.log(args);
+}
+
+/*
+function destroyer(arr) {
+  var args = Array.prototype.slice.call(arguments);
+  
+  console.log(args);
+  console.log(arr);
+  var argsp = args.slice(1);
+  console.log(argsp);
+
+  var arrA = arr.filter(function(val){
+        return val!=argsp[0];
+  });
+
+  console.log(arrA);
+
+  var arrB = arrA.filter(function(val){
+    return val != argsp[1];
+  });
+  console.log(arrB);
+  return arrB;
+}
+*/
+function destroyer(arr) {
+  var args = Array.prototype.slice.call(arguments);
+  
+  console.log(args);
+  console.log(arr);
+  var argsp = args.slice(1);
+  console.log(argsp);
+
+  
+  for(i=0; i < argsp.length; i++){
+     arr = arr.filter(function(val){
+        return val!=argsp[i];
+    }); 
+  }
+
+
+  console.log(arr);
+  return arr;
+}
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
